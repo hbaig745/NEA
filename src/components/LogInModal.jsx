7d322ceabcefb1569e07d5776c134144631fb400
hbaig745 <94379637+hbaig745@users.noreply.github.com>
2022-12-12 23:40:09 +0000

    Added Modals
    Added the login and register modals

---
+++ src/components/LogInModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./Modal.module.css";
+import { RiCloseLine } from "react-icons/ri";
+
+const LogInModal = ({ setIsOpen }) => {
+  return (
+    <>
+      <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+      <div className={styles.centered}>
+        <div className={styles.modal}>
+          <div className={styles.modalHeader}>
+            <h5 className={styles.heading}>Dialog</h5>
+          </div>
+          <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
+          </button>
+          <div className={styles.modalContent}>
+            Are you sure you want to delete the item?
+          </div>
+          <div className={styles.modalActions}>
+            <div className={styles.actionsContainer}>
+              <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
+                Delete
+              </button>
+              <button
+                className={styles.cancelBtn}
+                onClick={() => setIsOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LogInModal;
+//referenced from https://dev.to/franciscomendes10866/how-to-create-a-modal-in-react-3coc 
